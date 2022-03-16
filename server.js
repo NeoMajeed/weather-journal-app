117d8dcc71dbb7e719b57fbc72184f64ecb5bfe7
Ubdo15 <ubdo15@egmail.com>
2022-03-16 21:17:53 +0300

    setup server || app done

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const port = 8080;
 
 // Require Express to run server and routes
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const port = 8080;
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
+const bodyParser = require("body-parser");
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 // Cors for cross origin allowance
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
@@ -27,3 +28,21 @@ app.use(express.static('website'));
 app.listen(port, ()=>{
     console.log("server working on port: " + port);
 })
+
+// Get route
+app.get("/all", (req,res)=>{
+    res.send(projectData);
+})
+
+
+// Post route
+app.post("/add", (req,res)=>{
+    projectData.city = req.body.city;
+    projectData.country = req.body.country;
+    projectData.date = req.body.date;
+    projectData.temp = req.body.temp;
+    projectData.feeling = req.body.feeling;
+    res.send(projectData);
+})
+
+
--- website/app.js
+++ website/app.js
@@ -1,18 +1,82 @@
+
 /* Global Variables */
 const apiKey = "ca7ac655b2e15c8d34736322028a2e6f";
 const openWeather = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const zip = document.querySelector("#zip").value;
-const generate = document.querySelector("#generate")
+const zip = document.querySelector("#zip");
+const feelings = document.querySelector("#feelings");
+const date = document.querySelector("#date");
+const temp = document.querySelector("#temp");
+const content = document.querySelector("#content");
+const city = document.querySelector("#city");
+const generate = document.querySelector("#generate");
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-console.log(newDate);
 
 
+//fetch data
+const postData = async (url = "", data = {})=>{
+    const response = await fetch(url,{
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)   
+    })
+    try {
+      const newData = await response.json();
+      return newData;
+    } catch (error) {
+      console.log("error", error);
+    }
+}
 
-// start events 
+// get data from the server
+const retrieveData = async () =>{
+ const request = await fetch('/all');
+ try {
+ // Transform into JSON
+ const allData = await request.json()
+ console.log(allData)
+ // Write updated data to DOM elements
+ city.innerHTML = 'City: ' + allData.city + ", " + allData.country;
+ temp.innerHTML = 'Temperature: ' + Math.round(allData.temp) + ' °C';
+ content.innerHTML = 'Feeling: ' + allData.feeling;
+ date.innerHTML = 'Date: ' + allData.date;
+ }
+ catch(error) {
+   console.log("error", error);
+ }
+}
+
+// get data from Api
+const getApiData=async(url)=>{
+    const response = await fetch(url)
+    try {   
+     const data =await response.json();  
+     return data;
+    }catch (error){  
+     console.log("error",error);
+    }   
+}
+
+
+// Click Event 
 generate.addEventListener("click", ()=>{
-    const URL = `${openWeather}${zip}&appid=${apiKey}&units=metric`;
-    console.log(URL);
+  if(isNaN(zip.value) || zip.value == ''){
+    alert("Please Enter a zip code")
+  } else{
+    const URL = `${openWeather}${zip.value}&appid=${apiKey}&units=metric`;
+    // send data to the server
+    getApiData(URL)
+    .then(data => postData("/add", {city:data.name, country:data.sys.country, date:newDate, temp:data.main.temp, feeling:feelings.value})
+    // get data from the server and update the DOM 
+    .then(retrieveData())
+  );
+  }
 })
 
